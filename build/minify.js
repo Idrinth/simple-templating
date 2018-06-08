28a1e3293506aa091918382a3abaa55d06fa8b3a
Björn Büttner <idrinth@users.noreply.github.com>
2018-06-08 21:09:21 +0200

    simplifiing minify

--- build/minify.js
+++ build/minify.js
@@ -1,78 +1,81 @@
 const minify = require("uglify-es").minify;
 const fs = require("fs");
 
-let minifyFile = (id, reserved, globals) => {
-    let log = (name, file) => {
-        let size = fs.statSync(file).size;
-        console.log(name+": "+size+"b, "+(Math.ceil(1000*size/baseSize)/10)+"%");
-    };
-    reserved = reserved?reserved:[];
-    let min = fs.readFileSync("./src/template.js", {encoding: "utf-8"});
-    for (let key in globals) {
-        min = min.replace(key, "\""+globals[key]+"\"");
-    }
-    let len = min.length;
-    const config = {
-        ecma: 6,
-        compress: {
-            passes: 1
-        },
-        mangle: {
-            properties: {
-                reserved
-            }
-        },
-        output: {
-            beautify: false
-        }
-    };
-    do {
-        len = min.length;
-        min = minify(
-            min
-                .replace(/(case([^:'"]|"[^"]"|'[^']')+?:)+default:/, "default:")
-                .replace(/void 0!==(\"[^\"]*\"|'[^']*'|{.*?}|\[.*?\])\?/, "true?"),
-            config
-        )
-        .code;
-    } while (len !== min.length);
-    let path = "./min/"+id+".min.js";
-    fs.writeFileSync(path, min, {encoding: "utf-8"});
-    log(id?id:"base", path);
-};
-const reserved = ["exports","Template","render"];
 const baseSize = fs.statSync("./src/template.js").size;
-
-/**
- * Minifiing files
- */
-minifyFile(
-    "node",
-    reserved,
-    {
-        "typeof module": "object",
-        "typeof module.exports": "object",
-        "typeof window": "undefined",
-        "typeof self": "undefined",
-        "typeof define": "undefined"
+const config = {
+    ecma: 6,
+    compress: {
+        passes: 1
+    },
+    mangle: {
+        properties: {
+            reserved: ["exports", "Template", "render"]
+        }
+    },
+    output: {
+        beautify: false
     }
-);
-minifyFile(
-    "browser",
-    reserved,
-    {
+};
+
+((sets) => {
+    /**
+     * @param {string} id
+     * @param {string} file
+     * @return {void}
+     */
+    const log = (id, file) => {
+        let size = fs.statSync(file).size;
+        console.log(id+": "+size+"b, "+(Math.ceil(1000*size/baseSize)/10)+"%");
+    };
+    /**
+     * @param {string} id
+     * @param {type} reserved
+     * @param {type} globals
+     * @return {undefined}
+     */
+    const minifyFile = (id, globals) => {
+        let min = fs.readFileSync("./src/template.js", {encoding: "utf-8"});
+        for (let key in globals) {
+            min = min.replace(new RegExp(key, 'g'), "\""+globals[key]+"\"");
+        }
+        let len = min.length;
+        do {
+            len = min.length;
+            min = minify(
+                min
+                    .replace(/(case([^:'"]|"[^"]"|'[^']')+?:)+default:/, "default:")
+                    .replace(/void 0!==(\"[^\"]*\"|'[^']*'|{.*?}|\[.*?\])\?/, "true?"),
+                config
+            )
+            .code;
+        } while (len !== min.length);
+        let path = "./min/"+id+".min.js";
+        fs.writeFileSync(path, min, {encoding: "utf-8"});
+        log(id, path);
+    };
+    /**
+     * Minifiing files
+     */
+    for (let type in sets) {
+        minifyFile(type, sets[type]);
+    }
+})({
+    "node": {
+        "typeof module\\.exports": "object",
+        "typeof module": "object",
+        "typeof window": "undefined",
+        "typeof self": "object",
+        "typeof define": "undefined"
+    },
+    "browser": {
         "typeof module": "undefined",
         "typeof define": "undefined",
         "typeof window": "object"
-    }
-);
-minifyFile(
-    "amd",
-    reserved,
-    {
+    },
+    "amd": {
         "typeof module": "undefined",
         "typeof define": "function",
-        "define.amd": "1"
-    }
-);
-minifyFile("base", reserved, {});
+        "define\\.amd": "1"
+    },
+    "base": {}
+});
